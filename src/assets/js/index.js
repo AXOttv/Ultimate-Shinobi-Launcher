322cc6932bbafe18de08dd5b4be11f2aa7b93b82
AXO <1707pac@gmail.com>
2025-11-29 22:58:21 +0100

    update

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "AXO" },
+            { "message": "Salut je suis du code.", "author": "AXO" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "AXO" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
